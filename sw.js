7c26e3b8c74f9084b93e291ce52662eb6494c365
andreasbovens <abovens@gmail.com>
2015-11-16 21:13:53 -0800

    added SW

---
+++ sw.js
@@ -0,0 +1,57 @@
+/*
+ * Script slightly modified for Inbox Attack.
+ */
+
+/*
+ *
+ *  Air Horner
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+
+// Version 0.57
+
+importScripts('/inbox-attack/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('inboxattack').then(function(cache) {
+			return cache.addAll([
+				'/inbox-attack/',
+				'/inbox-attack/index.html',
+				'/inbox-attack/index.html?home=true',
+				'/inbox-attack/?home=true',
+				'/inbox-attack/screen.css',
+				'/inbox-attack/index.svg'
+			]).then(function() {
+				return self.skipWaiting();
+			});
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log(event.request.url);
+
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
